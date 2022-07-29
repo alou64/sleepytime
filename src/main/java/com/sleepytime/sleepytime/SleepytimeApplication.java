39d9ed4888e8dbc08af9afb4da40825266ad0641
alou64 <alou64@protonmail.com>
2022-07-29 08:14:21 -0700

    initial commit

---
+++ src/main/java/com/sleepytime/sleepytime/SleepytimeApplication.java
@@ -0,0 +1,13 @@
+package com.sleepytime.sleepytime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SleepytimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SleepytimeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sleepytime/sleepytime/SleepytimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sleepytime.sleepytime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SleepytimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
